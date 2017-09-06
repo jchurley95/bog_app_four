c21cf312d79cdbe4151448b60dcaa29737073f78
jchurley95 <jchurley95@gmail.com>
2017-09-06 15:31:14 -0400

    Edit and new pages are working, added partials directory and creature form in the directory which is rendered by edit and new

--- app/controllers/creatures_controller.rb
+++ app/controllers/creatures_controller.rb
@@ -8,8 +8,40 @@ class CreaturesController < ApplicationController
   end
 
   def edit
+    @creature = Creature.find params[:id]
   end
 
   def new
+    @creature = Creature.new
+  end
+
+  def create 
+    @creature = Creature.new(creature_params)
+
+    if @creature.save
+      redirect_to creatures_path
+    end
+  end
+
+  def update
+    @creature = Creature.find params[:id]
+
+    if @creature.update(creature_params)
+      redirect_to creature_path(@creature)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @creature = Creature.find params[:id]
+    @creature.destroy
+
+    redirect_to creatures_path
+  end 
+
+  private 
+  def creature_params
+    creature_params = params.require(:creature).permit(:name, :description)
   end
 end
