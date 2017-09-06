03ad826e16b67662aac357d010ee4982608d1774
jchurley95 <jchurley95@gmail.com>
2017-09-06 15:00:11 -0400

    First commit, created controller and model for Creatures

---
+++ app/controllers/creatures_controller.rb
@@ -0,0 +1,13 @@
+class CreaturesController < ApplicationController
+  def show
+  end
+
+  def index
+  end
+
+  def edit
+  end
+
+  def new
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  get 'creatures/show'
+
+  get 'creatures/index'
+
+  get 'creatures/edit'
+
+  get 'creatures/new'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
